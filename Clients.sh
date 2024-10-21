8159bb738d8852b7958715f6f32afe6c7a0f611b
juanC773 <115520140+juanC773@users.noreply.github.com>
2024-10-21 11:19:11 -0500

    status v3

--- Clients.sh
+++ Clients.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-hosts=("xhgrid1" "xhgrid2" "xhgrid3" "xhgrid4" "xhgrid5" "xhgrid6" "xhgrid7" "xhgrid8" "xhgrid9" "xhgrid10" "xhgrid11" "xhgrid12" "xhgrid13" "xhgrid14" "xhgrid15")
+hosts=("hgrid1" "hgrid2" "xhgrid3" "xhgrid4" "xhgrid5" "xhgrid6" "xhgrid7" "xhgrid8" "xhgrid9" "xhgrid10" "xhgrid11" "xhgrid12" "xhgrid13" "xhgrid14" "xhgrid15")
 
 
 for host in "${hosts[@]}"; do
-  scp -o StrictHostKeyChecking=no -r . swarch@xhgrid2:~/Callback
+  scp -o StrictHostKeyChecking=no -r . "swarch@x$host:~/Callback"
   swarch
-  ssh swarch@xhgrid2
+  ssh "swarch@x$host"
   swarch
   cd Callback
   chhmod +x gradlew
@@ -16,9 +16,8 @@ for host in "${hosts[@]}"; do
   unzip client.jar -d ./client
   rm client.jar
   cd Client
-  "vim"
-  zip
-  jar cvf Client.jar
+  sed -i 's/localhost/'$host'/g' config.properties
+  jar cvf Client
   rm -r Client
   java -jar Client.jar
   !1000
