1bb2721630a13cb1a64dc5d2f40a4b4650172c91
AlejandroTorres05 <alejandro.t.s.321@gmail.com>
2024-10-25 04:44:15 -0500

    bash

--- Clients.sh
+++ Clients.sh
@@ -1,30 +1,69 @@
-#!/bin/bash
+#!/usr/bin/expect -f
 
-hosts=("hgrid1" "hgrid2" "hgrid3" "hgrid4" "hgrid5" "hgrid6" "hgrid7" "hgrid8" "hgrid9" "hgrid10" "hgrid11" "hgrid12" "hgrid13" "hgrid14" "hgrid15")
+# Configuración de variables
+set timeout -1
+set password "swarch"
+set hosts {hgrid2 hgrid3 hgrid4 hgrid5 hgrid6 hgrid7 hgrid8 hgrid9 hgrid10 hgrid11 hgrid12 hgrid13 hgrid14 hgrid15 hgrid16 hgrid17 hgrid18 hgrid19}
 
-for host in "${hosts[@]}"; do
-  scp -o StrictHostKeyChecking=no -r . "swarch@x$host:~/Callback"
-  swarch
-  ssh "swarch@x$host"
-  swarch
-  cd Callback
-  chhmod +x gradlew
-  ./gradlew build
-  cd ./client/build/libs
-  mkdir client
-  unzip client.jar -d ./client
-  rm client.jar
-  cd client
-  sed -i 's/localhost/'$host'/g' client.config
-  jar cvf client
-  rm -r client
-  java -jar Client.jar
-  1000
-  
-done
+# Copiar inicialmente a xhgrid1
+spawn scp -o StrictHostKeyChecking=no -r . swarch@xhgrid1:~/Callback
+expect {
+    "password:" { send "$password\r" }
+    timeout { puts "Timeout en SCP inicial"; exit 1 }
+}
+expect eof
 
-for host in "${hosts[@]}"; do
-  ssh "swarch@x$host" 
-  swarch 
-  rm -r Callback
-done
+# Conectar a xhgrid20 y compilar
+spawn ssh -o StrictHostKeyChecking=no swarch@xhgrid1
+expect "password:" { send "$password\r" }
+expect "$ " { send "cd Callback\r" }
+expect "$ " { send "chmod +x ./gradlew\r" }
+expect "$ " { send "./gradlew build\r" }
+expect "$ "
+
+# Iterar sobre cada host
+foreach host $hosts {
+    # Copiar archivos al host
+    spawn scp -o StrictHostKeyChecking=no -r . swarch@$host:~/Callback
+    expect {
+        "password:" { send "$password\r" }
+        timeout { puts "Timeout en SCP para $host"; continue }
+    }
+    expect eof
+    
+    # Conectar y ejecutar comandos en el host
+    spawn ssh -o StrictHostKeyChecking=no swarch@$host
+    expect "password:" { send "$password\r" }
+    
+    # Preparar el cliente
+    expect "$ " { send "cd ./client/build/libs\r" }
+    expect "$ " { send "mkdir client\r" }
+    expect "$ " { send "unzip client.jar -d ./client\r" }
+    expect "$ " { send "rm -r client.jar\r" }
+    expect "$ " { send "cd client\r" }
+    expect "$ " { send "sed -i 's/localhost/$host/g' config.client\r" }
+    expect "$ " { send "cd ..\r" }
+    expect "$ " { send "jar cfm client.jar client/META-INF/MANIFEST.MF -C client .\r" }
+    expect "$ " { send "rm -r client\r" }
+    expect "$ " { send "java -jar client.jar\r" }
+    expect "222222"
+    expect "$ " { send "exit\r" }
+    expect eof
+}
+
+# Limpiar Callback local
+spawn rm -r Callback
+
+# Esperar un tiempo prudencial para las pruebas
+puts "Esperando 5 minutos para que finalicen las pruebas..."
+sleep 300
+
+# Limpiar Callback en hosts remotos y matar los procesos java
+foreach host $hosts {
+    spawn ssh -o StrictHostKeyChecking=no swarch@$host
+    expect "password:" { send "$password\r" }
+    expect "$ " { send "pkill -f 'java -jar client.jar'\r" }
+    expect "$ " { send "rm -r Callback\r" }
+    expect "$ " { send "exit\r" }
+    expect eof
+}
--- Server.sh
+++ Server.sh
@@ -1,16 +1,20 @@
-#!/bin/bash
-./gradlew clean build
-scp -r -o StrictHostKeyChecking=no -r ./server/build/libs/. swarch@xhgrid20:~/Callback
-swarch
-ssh -o StrictHostKeyChecking=no swarch@xhgrid20
-swarch
-cd Callback
-mkdir server
-unzip server.jar -d ./server
-rm -r server.jar
-cd server
-sed -i 's/localhost/'192.168.131.140'/g' config.server
-cd ..
-jar cfm server.jar server/META-INF/MANIFEST.MF -C server .
-rm -r server
-java -jar server 
+#!/usr/bin/expect -f
+
+# Configura el tiempo de espera y la contraseña
+set timeout -1
+set password "swarch"
+
+# Enviar el directorio Callback al host xhgrid20
+spawn scp -o StrictHostKeyChecking=no -r . swarch@xhgrid20:~/Callback
+expect "password:" { send "$password\r" }
+expect eof
+
+# Conectarse a xhgrid20
+spawn ssh -o StrictHostKeyChecking=no swarch@xhgrid20
+expect "password:" { send "$password\r" }
+expect "$ " { send "cd Callback && chmod +x ./gradlew && ./gradlew build\r" }
+expect "$ " { send "cd ./server/build/libs && mkdir server && unzip server.jar -d ./server && rm -r server.jar\r" }
+expect "$ " { send "cd server && sed -i 's/localhost/192.168.131.140/g' config.server && cd ..\r" }
+expect "$ " { send "jar cfm server.jar server/META-INF/MANIFEST.MF -C server . && rm -r server\r" }
+expect "$ " { send "java -jar server.jar\r" }
+expect eof
